4654c1d7df4b2ff6826af8b235c5946b380aff4f
barthr <info@bartfokker.com>
2023-07-14 11:50:04 +0200

    Fix plugins + add some easy moving between buffers

--- nvim/lua/custom/packer.lua
+++ nvim/lua/custom/packer.lua
@@ -3,33 +3,39 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-	-- Packer can manage itself
-	use ('wbthomason/packer.nvim')
+    -- Packer can manage itself
+    use('wbthomason/packer.nvim')
 
-	use { 'nvim-telescope/telescope.nvim', tag = '0.1.2', branch = '0.1.x', requires = { {'nvim-lua/plenary.nvim'} } }
-	use { "ellisonleao/gruvbox.nvim" }
+    use { 'nvim-telescope/telescope.nvim', tag = '0.1.2', branch = '0.1.x', requires = { { 'nvim-lua/plenary.nvim' } } }
+    use { "ellisonleao/gruvbox.nvim" }
 
-	use ('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
-	use ('nvim-treesitter/playground')
-	use ('mbbill/undotree')
-	use ('tpope/vim-fugitive')
-	use {
-		'VonHeikemen/lsp-zero.nvim',
-		branch = 'v2.x',
-		requires = {
-			-- LSP Support
-			{'neovim/nvim-lspconfig'},             -- Required
-			{                                      -- Optional
-			'williamboman/mason.nvim',
-			run = function()
-				pcall(vim.cmd, 'MasonUpdate')
-			end,
-		},
-		{'williamboman/mason-lspconfig.nvim'}, -- Optional
-		-- Autocompletion
-		{'hrsh7th/nvim-cmp'},     -- Required
-		{'hrsh7th/cmp-nvim-lsp'}, -- Required
-		{'L3MON4D3/LuaSnip'},     -- Required
-	}
-}
+    use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
+    use('nvim-treesitter/playground')
+    use('mbbill/undotree')
+    use('tpope/vim-fugitive')
+    use('tpope/vim-surround')
+    use {
+        'nvim-lualine/lualine.nvim',
+        requires = { 'nvim-tree/nvim-web-devicons', opt = true }
+    }
+
+    use {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v2.x',
+        requires = {
+            -- LSP Support
+            { 'neovim/nvim-lspconfig' }, -- Required
+            {                 -- Optional
+                'williamboman/mason.nvim',
+                run = function()
+                    pcall(vim.cmd, 'MasonUpdate')
+                end,
+            },
+            { 'williamboman/mason-lspconfig.nvim' }, -- Optional
+            -- Autocompletion
+            { 'hrsh7th/nvim-cmp' },      -- Required
+            { 'hrsh7th/cmp-nvim-lsp' },  -- Required
+            { 'L3MON4D3/LuaSnip' },      -- Required
+        }
+    }
 end)
--- nvim/lua/custom/remap.lua
+++ nvim/lua/custom/remap.lua
@@ -1,5 +1,12 @@
 vim.g.mapleader = ","
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
+-- Fix moving between buffers
+vim.keymap.set("n", "<C-j>", "<C-w>j")
+vim.keymap.set("n", "<C-k>", "<C-w>k")
+vim.keymap.set("n", "<C-l>", "<C-w>l")
+vim.keymap.set("n", "<C-h>", "<C-w>h")
+
+vim.keymap.set('n', '<A-1>', ":Lexplore<CR>")
 -- Fix annoying paste over highlight
 -- vim.keymap.set("x", "<leader>p", "\"_dP")
